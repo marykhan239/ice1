ba6549f61ca4d946b621d1a1551afe759a2e25dd
Maryam Khan <59967549+marykhan239@users.noreply.github.com>
2021-10-07 21:17:32 -0400

    Update CardTrick.java

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
@@ -10,6 +10,11 @@ public class CardTrick {
     public static void main(String[] args)
     {
        Card card=new Card();
+         String s= card.getSuit();
+       int v= card.getValue();
+      
+        System.out.println(s);
+        System.out.println(v);
     }
     
 }
