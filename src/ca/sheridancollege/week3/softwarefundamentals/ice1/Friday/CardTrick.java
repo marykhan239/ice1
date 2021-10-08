744bf6cb14f29342eeead7f1edcb822459d68dc3
maryam khan <khan521@sheridancollege.ca>
2021-10-07 21:00:31 -0400

    Second Commit

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
@@ -3,12 +3,13 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1.Friday;
 
 /**
  *
- * @author Ronak
+ * @author Ronak 
+ * @modifier Maryam Khan Student number: 991588777
  */
 public class CardTrick {
     public static void main(String[] args)
     {
-       
+       Card card=new Card();
     }
     
 }
